a61ae208d41cbae6b614ea756a1e213798410662
TeejMcSteez <teejmcsteez@gmail.com>
2026-06-15 22:48:43 -0400

    add filecheck

--- main.go
+++ main.go
@@ -15,9 +15,13 @@ func main() {
 	if len(os.Args) < 2 || len(os.Args) > 3 {
 		log.Fatal("Failed to provide configuration file for variables")
 	}
-	conf_file := os.Args[1]
-
-	if out, err := providers.JSearch(conf_file); err != nil {
+	arg := os.Args[1]
+	_, err := os.Stat(arg)
+	if arg == "-h" || arg == "--help" || arg == "-help" || err != nil {
+		fmt.Print("Write a YAML file containing:\nrapid_api_url: <URL>\nrapid_api_headers: \nContent-Type: Application\n<KEY>: <VALUE>\nrapid_api_params:\n- search\n- ?query=example%20query&\n- another=true\n\nRun the program with the path to the file `./sparechange conf.yml` it will use the information to fetch data with given parameters and output via text (1) or markdown (2)")
+		return
+	}
+	if out, err := providers.JSearch(arg); err != nil {
 		log.Fatalf("Failed to search JSearch Provider: %v", err)
 	} else {
 		fmt.Print("Choose output format: \n1: Text File\n2: Markdown\n")
